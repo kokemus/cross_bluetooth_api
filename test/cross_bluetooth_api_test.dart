47d0887ab669885476233b571d7644ee43c04771
kokemus <>
2026-08-05 20:55:03 +0300

    Fix tests

--- test/cross_bluetooth_api_test.dart
+++ test/cross_bluetooth_api_test.dart
@@ -4,23 +4,45 @@ import 'package:cross_bluetooth_api/cross_bluetooth_api.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('cross_bluetooth_api');
-
   TestWidgetsFlutterBinding.ensureInitialized();
+  final binaryMessenger =
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger;
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+    binaryMessenger.setMockMethodCallHandler(channel, (
+      MethodCall methodCall,
+    ) async {
+      return {'id': 'device-42', 'name': 'Test Device'};
     });
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    binaryMessenger.setMockMethodCallHandler(channel, null);
   });
 
-  test('getPlatformVersion', () async {
+  test('requestDevice returns parsed device', () async {
+    final device = await Bluetooth.requestDevice(
+      RequestDeviceOptions(acceptAllDevices: true),
+    );
+
+    expect(device.id, 'device-42');
+    expect(device.name, 'Test Device');
+  });
+
+  test('requestDevice maps platform errors', () async {
+    binaryMessenger.setMockMethodCallHandler(channel, (
+      MethodCall methodCall,
+    ) async {
+      throw PlatformException(
+        code: '1',
+        message: 'NotFoundError: no device found',
+      );
+    });
+
     expect(
-        await Bluetooth.requestDevice(
-            RequestDeviceOptions(acceptAllDevices: true)),
-        '42');
+      () =>
+          Bluetooth.requestDevice(RequestDeviceOptions(acceptAllDevices: true)),
+      throwsA(isA<NotFoundError>()),
+    );
   });
 }
